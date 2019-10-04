19ed570a0c2e79b9e99b71fd57cc1e8cfc9ef5aa
Bewvnv <56114615+Bewvnv@users.noreply.github.com>
2019-10-04 21:53:14 +0700

    Exercise4_Nuttha_V.py

---
+++ Exercise4_Nuttha_V.py
@@ -0,0 +1,11 @@
+score_FE=68.00
+score_GB=72.00
+score_ICS=84.00
+score_CP=75.00
+print("---Your Score---")
+print("Foundation English :",score_FE)
+print("General Business :",score_GB)
+print("Introduction to Computer System :",score_ICS)
+print("Computer Programing :",score_CP)
+
+
