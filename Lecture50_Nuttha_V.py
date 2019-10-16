9e1e45782531cd8c1331f04cb468aa100fbacf6f
Bewvnv <56114615+Bewvnv@users.noreply.github.com>
2019-10-16 17:02:09 +0700

    Lecture50

---
+++ Lecture50_Nuttha_V.py
@@ -0,0 +1,15 @@
+def addNumber(x,y):
+    print(x,"+",y,"=",x+y)
+def subtractNumber(x,y):
+    print(x,"-",y,"=",x-y)
+def multiplyNumber(x,y):
+    print(x,"*",y,"=",x*y)
+def divideNumber(x,y):
+    print(x,"/",y,"=",x/y)
+
+x = float(input("first number"))
+y = float(input("second number"))
+addNumber(x,y)
+subtractNumber(x,y)
+multiplyNumber(x,y)
+divideNumber(x,y)
